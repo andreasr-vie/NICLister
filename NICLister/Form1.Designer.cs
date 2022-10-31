9a85eb99261050bc74090764db2dd53897e34fc7
Andreas R <114869961+andreasr-vie@users.noreply.github.com>
2022-10-31 23:07:07 +0100

    Add project files.

---
+++ NICLister/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace NICLister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColumnNIC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnMAC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnNIC,
+            this.ColumnMAC});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 426);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ColumnNIC
+            // 
+            this.ColumnNIC.HeaderText = "NIC";
+            this.ColumnNIC.Name = "ColumnNIC";
+            this.ColumnNIC.ReadOnly = true;
+            // 
+            // ColumnMAC
+            // 
+            this.ColumnMAC.HeaderText = "MAC";
+            this.ColumnMAC.Name = "ColumnMAC";
+            this.ColumnMAC.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNIC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnMAC;
+    }
+}
+
---
+++ NICLister/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NICLister
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 7;
+            dataGridView1.Columns[0].Name = "NIC";
+            dataGridView1.Columns[1].Name = "Name";
+            dataGridView1.Columns[2].Name = "MAC";
+            dataGridView1.Columns[3].Name = "IPv4";
+            dataGridView1.Columns[4].Name = "IPv6";
+            dataGridView1.Columns[5].Name = "DNSv4";
+            dataGridView1.Columns[6].Name = "DNSv6";
+
+            string[] ExcludeIFList = { "Wintun", "TAP", "Loopback", "Microsoft Wi-Fi", "VMware" };
+            bool ContainsIF = false;
+
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in adapters)
+            {
+                foreach (string x in ExcludeIFList)
+                {
+                    if (adapter.Description.Contains(x))
+                    {
+                        ContainsIF = true;
+                    }
+                }
+                if (!ContainsIF)
+                {
+                    var ipv4 = new List<string>();
+                    var ipv6 = new List<string>();
+                    var dnsv4 = new List<string>();
+                    var dnsv6 = new List<string>();
+                    IPInterfaceProperties properties = adapter.GetIPProperties();
+                    foreach (IPAddressInformation unicast in properties.UnicastAddresses)
+                    {
+                        if (IPtype(unicast.Address.ToString()) == 6)
+                        {
+                            if (!(IsIPv6LocalLink(unicast.Address.ToString())))
+                                ipv6.Add(unicast.Address.ToString());
+                        }
+                        else if (IPtype(unicast.Address.ToString()) == 4)
+                        {
+                            ipv4.Add(unicast.Address.ToString());
+                        }
+                    }
+
+                    IPAddressCollection dnsServers = properties.DnsAddresses;
+                    if (dnsServers.Count > 0)
+                    {
+                        foreach (IPAddress d in dnsServers)
+                        {
+                            if (IPtype(d.ToString()) == 4)
+                            {
+                                dnsv4.Add(d.ToString());
+                            }
+                            else if (IPtype(d.ToString()) == 6)
+                            {
+                                dnsv6.Add(d.ToString());
+                            }
+                        }
+                    }
+
+                    var ipv4list = String.Join(", ", ipv4.ToArray());
+                    var ipv6list = String.Join(", ", ipv6.ToArray());
+                    var dnsv4List = String.Join("\r\n", dnsv4.ToArray()); 
+                    var dnsv6List = String.Join("\r\n", dnsv6.ToArray());
+
+                    dataGridView1.Rows.Add(adapter.Description, adapter.Name, FormatMAC(System.Convert.ToString(adapter.GetPhysicalAddress())), ipv4list, ipv6list, dnsv4List, dnsv6List);
+                }
+                ContainsIF = false;
+            }
+            dataGridView1.AutoResizeColumns();
+            dataGridView1.AutoResizeRows();
+        }
+
+        public bool IsIPv6LocalLink(string input)
+        {
+            Regex rx = new Regex(@"fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}$");
+            if (rx.IsMatch(input))
+                return true;
+            else
+                return false;
+        }
+
+        public int IPtype(string input)
+        {
+            IPAddress address;
+            if (IPAddress.TryParse(input, out address))
+            {
+                switch(address.AddressFamily)
+                {
+                    case System.Net.Sockets.AddressFamily.InterNetwork:
+                        //we have IPv4
+                        return 4;
+                    case System.Net.Sockets.AddressFamily.InterNetworkV6:
+                        //we have IPv6
+                        return 6;
+                    default:
+                        //PUH
+                        return 0;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public string FormatMAC(string input)
+        {
+            var regex = "(.{2})(.{2})(.{2})(.{2})(.{2})(.{2})";
+            var replace = "$1:$2:$3:$4:$5:$6";
+            var newformat = Regex.Replace(input, regex, replace);
+            return newformat;
+        }
+    }
+}
